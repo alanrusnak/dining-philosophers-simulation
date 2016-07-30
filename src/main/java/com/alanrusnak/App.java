568175d9ae1efa0191685cd8cacdbd4662ea3c9e
alanrusnak <alanrusnak9@gmail.com>
2016-07-30 13:15:13 +0100

    Initial commit

---
+++ src/main/java/com/alanrusnak/App.java
@@ -0,0 +1,13 @@
+package com.alanrusnak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
